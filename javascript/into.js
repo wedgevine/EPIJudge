2a5c58490708618c52df41a0d2e90b9923b6ab47
yljhsb <yljhsb@gmail.com>
2019-06-21 07:13:10 -0400

    Add javascript folder, supported by Code Runner

---
+++ javascript/into.js
@@ -0,0 +1,9 @@
+// https://github.com/getify/You-Dont-Know-JS
+// https://maximdenisov.gitbooks.io/you-don-t-know-js/content/
+
+console.log("hi")
+console.log("hi")
+console.log("hi")
+
+let a = 1
+console.log(a)
