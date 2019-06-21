c007d0ff9e6036c621278ee155d0e57e341c337e
yljhsb <yljhsb@gmail.com>
2019-06-20 20:36:37 -0400

    Add references

--- python/datatype.py
+++ python/datatype.py
@@ -1,3 +1,11 @@
+# https://docs.python.org/3/index.html
+# https://www.python.org/dev/peps/pep-0008/
+# Getting Started with Python https://docs.python-guide.org/
+# Automate the Boring Stuff with Python https://automatetheboringstuff.com/#toc
+# https://www.digitalocean.com/community/tutorial_series/how-to-code-in-python-3
+# Python 101 https://news.ycombinator.com/item?id=15932381
+# books: learning python, python crash course, python tricks, introducing python, effective python
+
 #%%
 msg = "Hello World"
 print(msg)
