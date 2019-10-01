65d54856af79ebb4ad653d232b6ee52f20b94224
zzhou <zzhou@skybitz.com>
2019-10-01 15:27:52 -0400

    buy and sell stock
    redo buy and sell stock for once
    working on buy and sell stock twice, thinking some generalized algo

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -15,8 +15,30 @@ def first(prices):
 
     return profit
 
+def second(prices):
+    
+    if not prices:
+        return 0.0
+
+    current, last = 1, len(prices) - 1
+    lowest_value, profit = prices[0], 0.0
+
+    # loop over the list
+    # always remember the lowest value seen so far, keep record for current profit
+    # whenever see a bigger value, minus the lowest to see if can get more profit
+    # whenever see a smaller value, compare with lowest to determin current lowest
+    while current <= last:
+        if prices[current] < prices[current - 1]:
+            lowest_value = min(lowest_value, prices[current])
+        if prices[current] > prices[current - 1]:
+            profit = max(profit, prices[current] - lowest_value)
+        current += 1
+
+    return profit
+
 def buy_and_sell_stock_once(prices):
-    return first(prices)
+    # return first(prices)
+    return second(prices)
 
 
 if __name__ == '__main__':
--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -16,7 +16,7 @@ def first(prices):
                 largest_profit, second_largest_profit = profit, largest_profit
             elif profit > second_largest_profit:
                 second_largest_profit = profit
-            print("\n", current_index, prices[current_index], profit, largest_profit, second_largest_profit)
+            # print("\n", current_index, prices[current_index], profit, largest_profit, second_largest_profit)
             profit = 0.0
         current_index += 1
 
@@ -26,6 +26,30 @@ def first(prices):
         second_largest_profit = profit
 
     return largest_profit + second_largest_profit
+
+def second(prices):
+
+    if not prices:
+        return 0.0
+
+    current, last = 1, len(prices) - 1
+    low, high = [0, 0], [0, 0]
+    current_low = 0
+    profit = 0.0
+
+    while current <= last:
+        if prices[current] < prices[current_low]:
+            current_low = current
+        if prices[current] > prices[current_low]:
+            if prices[high[0]] - prices[low[1]] <= prices[high[1]] - prices[current_low]:
+                high[0] = high[1]
+                low[1] = current_low
+                high[1] = current
+                profit = (prices[high[0]] - prices[low[0]])  + (prices[high[1]] - prices[low[1]])
+
+        current += 1
+
+    return profit
 
 def buy_and_sell_stock_twice(prices):
     return first(prices)
