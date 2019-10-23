24e7c24cadf0c9292d0745cff19eb884689c1f78
yljhsb <yljhsb@gmail.com>
2019-10-23 15:47:41 -0400

    LC88, merge sorted arrays

--- LC/88.merge-sorted-array.py
+++ LC/88.merge-sorted-array.py
@@ -10,6 +10,21 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        current1, current2 = m - 1, n - 1
+
+        while current2 >= 0:
+            if current1 >= 0:
+                if nums2[current2] > nums1[current1]:
+                    nums1[current1 + current2 + 1] = nums2[current2]
+                    current2 -= 1
+                else:
+                    nums1[current1 + current2 + 1] = nums1[current1]
+                    current1 -= 1
+            else:
+                nums1[current1 + current2 + 1] = nums2[current2]
+                current2 -= 1
+
+        return
         
 # @lc code=end
 
